a268c19c7868d3cf685203875c124f1189f4ab60
Indradeep-Pal <indradeep2002@gmail.com>
2023-12-29 11:19:18 +0530

    sample backend app

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express()
+const port= process.env.PORT
+app.get('/',(req,res)=>{
+    res.send("Hello ! Welcome to the Home Page")
+})
+app.get('/about', (req, res) => {
+    res.send("Hello ! Welcome to the About Page")
+})
+
+app.listen(port,()=>{
+    console.log(`App is started and running live at port ${port}`)
+})
