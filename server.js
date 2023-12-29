57512b377c17dce1c42eacfdba40c2fb755b4c51
Indradeep-Pal <indradeep2002@gmail.com>
2023-12-29 11:32:39 +0530

    Latest commit

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/',(req,res)=>{
 app.get('/about', (req, res) => {
     res.send("Hello ! Welcome to the About Page")
 })
+app.get('/contact', (req, res) => {
+    res.send("Hello ! Welcome to the Contact Us Page")
+})
 
 app.listen(port,()=>{
     console.log(`App is started and running live at port ${port}`)
